38e7ec1f5173130c142baf1766b5220d01e8d5e6
Aidan Clyens <aidanc@rogers.com>
2020-05-09 23:18:12 -0400

    Add time step multiplier for simulation

--- Assets/Scripts/NBodySimulation.cs
+++ Assets/Scripts/NBodySimulation.cs
@@ -1,13 +1,15 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NBodySimulation : MonoBehaviour {
+    public int timeMultiplier;
+
     GravitationalBody[] bodies;
 
     void Awake() {
         bodies = FindObjectsOfType<GravitationalBody>();
-        Time.fixedDeltaTime = Universe.timeStep;
+        Time.fixedDeltaTime = (1f / timeMultiplier) *Universe.timeStep;
     }
 
     void FixedUpdate() {
