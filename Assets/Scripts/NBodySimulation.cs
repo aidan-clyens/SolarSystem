2b327f8e6915741fb13aaca28d7592335e3e026d
Aidan Clyens <aidanc@rogers.com>
2020-05-09 22:23:01 -0400

    Add NBodySimulation script

---
+++ Assets/Scripts/NBodySimulation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NBodySimulation : MonoBehaviour {
+    public float timeStep;
+
+    GravitationalBody[] bodies;
+
+    void Start() {
+        bodies = FindObjectsOfType<GravitationalBody>();
+        Time.fixedDeltaTime = timeStep;
+    }
+
+    void FixedUpdate() {
+        foreach (GravitationalBody body in bodies) {
+            body.UpdatePosition(timeStep);
+        }
+    }
+}
