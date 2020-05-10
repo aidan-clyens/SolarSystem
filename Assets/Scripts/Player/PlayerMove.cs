c486ed4ac90661b6521b786214beee36e6c86112
Aidan Clyens <aidanc@rogers.com>
2020-05-10 15:27:54 -0400

    Add script to move Player

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    public CharacterController controller;
+    public Camera camera;
+    public float speed;
+
+    Vector3 velocity;
+
+    void Update() {
+        Move();
+    }
+
+    void Move() {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = camera.transform.forward * z + camera.transform.right * x;
+
+        controller.Move(move * speed * Time.deltaTime);
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
