a96e2ec00080f50c751b79d9d969ce856f995224
Aidan Clyens <aidanc@rogers.com>
2020-05-09 22:02:18 -0400

    Initial commit

---
+++ Assets/Scripts/GravitationalBody.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitationalBody : MonoBehaviour {
+    public Vector3 velocity;
+    public float mass;
+    public float radius;
+
+    public void UpdatePosition(float timeStep) {
+        GetComponent<RigidBody>().position += velocity * timeStep;
+    }
+}
