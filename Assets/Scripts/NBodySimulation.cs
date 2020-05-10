496691d54e1a6745523526e46addff30ce52e648
Aidan Clyens <aidanc@rogers.com>
2020-05-09 22:46:21 -0400

    Add constants to Universe static class

--- Assets/Scripts/NBodySimulation.cs
+++ Assets/Scripts/NBodySimulation.cs
@@ -1,20 +1,18 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NBodySimulation : MonoBehaviour {
-    public float timeStep;
-
     GravitationalBody[] bodies;
 
     void Awake() {
         bodies = FindObjectsOfType<GravitationalBody>();
-        Time.fixedDeltaTime = timeStep;
+        Time.fixedDeltaTime = Universe.timeStep;
     }
 
     void FixedUpdate() {
         foreach (GravitationalBody body in bodies) {
-            body.UpdatePosition(timeStep);
+            body.UpdateVelocity(bodies, Universe.timeStep);
         }
     }
 }
